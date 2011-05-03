4704c5a89ee99aa01126b9bdf3c70ec9b9524935
jasonhendriks <jasonhendriks@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-03 20:28:26 +0000

    v1.0
    git-svn-id: https://plugins.svn.wordpress.org/dynamic-dates/trunk@380364 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ dynamic-dates.php
@@ -0,0 +1,133 @@
+<?php
+/*
+
+		Plugin Name: Dynamic Dates
+		Plugin URI: http://www.jasonhendriks.com/programmer/dynamic-dates/
+		Description: A WordPress plugin that dynamically calculates dates in your post or page
+		Version: 1.0
+		Author: Jason Hendriks
+		Author URI: http://jasonhendriks.com/
+		License: GPL version 3 or any later version
+		
+		** Requires WordPress 2.7 **
+		
+		Copyright (C) 2011  Jason Hendriks
+		
+		This program is free software: you can redistribute it and/or modify
+		it under the terms of the GNU General Public License as published by
+		the Free Software Foundation, either version 3 of the License, or
+		(at your option) any later version.
+		
+		This program is distributed in the hope that it will be useful,
+		but WITHOUT ANY WARRANTY; without even the implied warranty of
+		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+		GNU General Public License for more details.
+		
+		You should have received a copy of the GNU General Public License
+		along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
+
+if(!class_exists("Dynamic_Dates")) {
+
+	/**
+	 * Dynamic_Dates.
+	 */
+	class Dynamic_Dates {
+			
+		function Dynamic_Dates() {
+			add_shortcode( "date", array( $this, "date_shortcode") );
+			add_shortcode( "dynamic-dates-version", array( $this, "version_shortcode") );
+			add_shortcode( "yesterday", array( $this, "yesterday_shortcode") );
+			add_shortcode( "today", array( $this, "today_shortcode") );
+			add_shortcode( "tomorrow", array( $this, "tomorrow_shortcode") );
+			add_shortcode( "last-month", array( $this, "last_month_shortcode") );
+			add_shortcode( "this-month", array( $this, "this_month_shortcode") );
+			add_shortcode( "next-month", array( $this, "next_month_shortcode") );
+			add_shortcode( "last-year", array( $this, "last_year_shortcode") );
+			add_shortcode( "this-year", array( $this, "this_year_shortcode") );
+			add_shortcode( "next-year", array( $this, "next_year_shortcode") );
+		}
+	
+		/**
+		 * Shortcode to return the current plugin version.
+		 * From http://code.garyjones.co.uk/get-wordpress-plugin-version/
+		 *
+		 * @return string Plugin version
+		 */
+		function version_shortcode() {
+			return $this->get_version();
+		}
+
+		/**
+		 * Shortcode to return the current plugin version.
+		 * From http://code.garyjones.co.uk/get-wordpress-plugin-version/
+		 *
+		 * @return string Plugin version
+		 */
+		function get_version() {
+			if ( ! function_exists( "get_plugins" ) ) {
+				require_once( ABSPATH . "wp-admin/includes/plugin.php" );
+			}
+			$plugin_folder = get_plugins( "/" . plugin_basename( dirname( __FILE__ ) ) );
+			$plugin_file = basename( ( __FILE__ ) );
+			return $plugin_folder[$plugin_file]["Version"];
+		}
+
+
+		function yesterday_shortcode() {
+			return $this->get_date("l", "-1 day");
+		}
+
+		function today_shortcode() {
+			return $this->get_date("l");
+		}
+
+		function tomorrow_shortcode() {
+			return $this->get_date("l", "+1 day");
+		}
+
+		function last_month_shortcode() {
+			return $this->get_date("F", "-1 month");
+		}
+
+		function this_month_shortcode() {
+			return $this->get_date("F");
+		}
+
+		function next_month_shortcode() {
+			return $this->get_date("F", "+1 month");
+		}
+
+		function last_year_shortcode() {
+			return $this->get_date("Y", "-1 year");
+		}
+
+		function this_year_shortcode() {
+			return $this->get_date("Y");
+		}
+
+		function next_year_shortcode() {
+			return $this->get_date("Y", "+1 year");
+		}
+
+		function date_shortcode($atts) {
+			extract(shortcode_atts(array(
+				"output" => "c",
+				"time" => "now"
+			), $atts));
+			return $this->get_date($output, $time);
+		}
+
+		function get_date($output="c", $time="now") {
+			return date($output, strtotime($time));
+		}
+	}
+}
+
+// start the meat and potatoes
+if (class_exists("Dynamic_Dates")) {
+    $dynamic_dates = new Dynamic_Dates();
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,67 @@
+=== Dynamic Dates ===
+Contributors: jasonhendriks
+Donate link: http://www.jasonhendriks.com/programmer/dynamic-dates/
+Tags: dynamic, date, dates, calculator
+Requires at least: 2.7
+Tested up to: 3.1.2
+Stable tag: 1.0
+
+A WordPress plugin that dynamically calculates dates in your post or page.
+
+== Description ==
+
+A WordPress plugin that dynamically calculates dates and times. Custom dates can be inserted in your post or page with short-codes.
+
+== Installation ==
+
+1. Download Dynamic Dates
+1. Unzip and upload the resulting folder to the '/wp-content/plugins/' directory
+1. Activate the plugin through the 'Plugins' menu in WordPress
+1. Place the shortcode in your posts and/or pages. A theme template function call is also available.
+
+**Detailed examples for use can be found at the [Dynamic Dates homepage](http://www.jasonhendriks.com/programmer/dynamic-dates/)**
+
+*Some simple examples:*
+
+>[yesterday]
+
+>[this-month]
+
+>[next-year]
+
+>[date output="F jS" time="this Sunday"]
+
+>[date output="Y" time="2 years ago"]
+
+>[date output="Y-m-d" time="march 4th -1 day +1 year"]
+
+== Frequently Asked Questions ==
+
+= How does it work? =
+
+Dynamic Dates uses the built-in PHP functions [date] and [strtotime] to parse and display custom dates.
+
+= What are the built-in shortcodes? =
+
+* [yesterday], [today], [tomorrow], [last-month], [this-month], [next-month], [last-year], [this-year], [next-year]
+
+= How can I create a customized date? =
+
+* Use the [date] shortcode. It accepts an output format option (see the PHP [Date Format Options](http://php.net/manual/en/function.date.php) page) and a time option which is a date/time string (see the PHP [Date and Time Format](http://www.php.net/manual/en/datetime.formats.php) page).
+
+= How can I contact the author? =
+
+Send me a [question or comment](http://www.jasonhendriks.com/contact/ "Contact Jason Hendriks") at my webpage.
+
+== Changelog ==
+
+= 0.1 =
+* Release date: 2011-05-02
+* First release
+
+== Upgrade Notice ==
+
+= 0.1 =
+
+The first version. Yay!
+
